6c86d62f081ccebdadd2686f139711a831435992
Hocine Benouddane <hocine20992@gmail.com>
2023-09-06 18:00:48 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+  static sanitize(string) {
+    return string.replace(/[^a-zA-Z0-9\-\'\s]/g, "");
+  }
+  static titleize(sentence) {
+    let words = sentence.split(" ");
+    let excludedWords = [
+      "the",
+      "a",
+      "an",
+      "but",
+      "of",
+      "and",
+      "for",
+      "at",
+      "by",
+      "from",
+    ];
+    let result = [];
+
+    let capitalizedWords = words.map((word, index) => {
+      if (index === 0 || !excludedWords.includes(word.toLowerCase())) {
+        result.push(this.capitalize(word));
+      } else {
+        result.push(word.toLowerCase());
+      }
+    });
+    return result.join(" ");
+  }
 }
